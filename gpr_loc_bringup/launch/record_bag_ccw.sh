c895ffc83b2ee7b38642f621e8b49a2fb0653b5f
Roman Stadlhuber <rs@d-aria.at>
2022-12-14 14:00:46 +0000

    add scripts to record rosbag data

---
+++ gpr_loc_bringup/launch/record_bag_ccw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rosbag record \
+# filename
+-o taurob_ccw.bag \
+# control signal "u"
+/taurob_tracker/cmd_vel_raw \
+# raw IMU data
+/taurob_tracker/imu/data \
+# precomputed mechanical odometry (from gazebo plugin)
+/odom \
+# ground truth odometry (from gazebo plugin)
+/ground_truth/odom \
