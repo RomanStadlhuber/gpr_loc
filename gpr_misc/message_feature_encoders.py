f7ed78e9984ab6f4c81d638729913e87095f54c4
Roman Stadlhuber <rs@d-aria.at>
2022-12-15 14:05:01 +0100

    implement encoder fns for relevant message types

--- gpr_misc/message_feature_encoders.py
+++ gpr_misc/message_feature_encoders.py
@@ -5,19 +5,51 @@ from rosbags.typesys.types import (
     geometry_msgs__msg__Twist as Twist,
     nav_msgs__msg__Odometry as Odometry,
 )
-from typing import List, Optional
+from typing import List, Optional, Tuple, Any
 
 
-def __encode_imu(_: Imu, topic: str) -> List[GPFeature]:
-    return [GPFeature(f"imu ({topic})", 123)]
+def __to_gp_features(xs: List[Tuple[str, Any]]) -> List[GPFeature]:
+    """convert a list of `(name, value)` tuples to GP features"""
+    return list(map(lambda tup: GPFeature(column_name=tup[0], value=tup[1]), xs))
 
 
-def __encode_odometry(_: Odometry, topic: str) -> List[GPFeature]:
-    return [GPFeature(f"odometry ({topic})", 456)]
+def __encode_imu(msg: Imu, topic: str) -> List[GPFeature]:
+    return __to_gp_features(
+        [
+            (f"imu.lin_acc.x ({topic})", msg.linear_acceleration.x),
+            (f"imu.lin_acc.y ({topic})", msg.linear_acceleration.y),
+            (f"imu.lin_acc.z ({topic})", msg.linear_acceleration.z),
+            (f"imu_ang_vel.x ({topic})", msg.angular_velocity.x),
+            (f"imu_ang_vel.y ({topic})", msg.angular_velocity.y),
+            (f"imu_ang_vel.z ({topic})", msg.angular_velocity.z),
+        ]
+    )
 
 
-def __encode_twist(_: Twist, topic: str) -> List[GPFeature]:
-    return [GPFeature(f"twist ({topic})", 789)]
+def __encode_odometry(msg: Odometry, topic: str) -> List[GPFeature]:
+    return __to_gp_features(
+        [
+            (f"odom.twist.lin.x ({topic})", msg.twist.twist.linear.x),
+            (f"odom.twist.lin.y ({topic})", msg.twist.twist.linear.y),
+            # (f"odom.twist.lin.z ({topic})", msg.twist.twist.linear.z),
+            # (f"odom.twist.ang.x ({topic})", msg.twist.twist.angular.x),
+            # (f"odom.twist.ang.y ({topic})", msg.twist.twist.angular.y),
+            (f"odom.twist.ang.z ({topic})", msg.twist.twist.angular.z),
+        ]
+    )
+
+
+def __encode_twist(msg: Twist, topic: str) -> List[GPFeature]:
+    return __to_gp_features(
+        [
+            (f"twist.lin.x ({topic})", msg.linear.x),
+            (f"twist.lin.y ({topic})", msg.linear.y),
+            # (f"twist.lin.z ({topic})", msg.linear.z),
+            # (f"twist.ang.x ({topic})", msg.angular.x),
+            # (f"twist.ang.y ({topic})", msg.angular.y),
+            (f"twist.ang.z ({topic})", msg.angular.z),
+        ]
+    )
 
 
 ENCODER_MAP = {
