update and fix GPDataset functions fix incorrect scaling calls and logic, now returning two scalers the rescaling also takes two scalars add a function to print information about the dataset add functions to obtain X matrix and y vector from dataframes

gpr_misc/helper_types.py
@@ -1,8 +1,10 @@
-from typing import TypeVar, Callable, Any, List
+from typing import TypeVar, Callable, Any, List, Tuple
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from sklearn.preprocessing import StandardScaler
+from pprint import pformat
 import pandas as pd
+import numpy as np
 import pathlib
 
 TRosMsg = TypeVar("TRosMsg")
@@ -29,6 +31,39 @@ class GPDataset:
     # set of all label vectors generated from a rosbag, needs to be separated into its columns to obtain training data
     labels: pd.DataFrame
 
+    def __init__(
+        self, dataset_folder: pathlib.Path, data_file_prefix_name: str
+    ) -> None:
+        """An optional constructor that uses the filename prefix to load features and labels"""
+
+        features_dir = dataset_folder.joinpath(f"{data_file_prefix_name}_features.csv")
+        labels_dir = dataset_folder.joinpath(f"{data_file_prefix_name}_labels.csv")
+
+        if (
+            features_dir.exists()
+            and features_dir.is_file()
+            and labels_dir.exists()
+            and labels_dir.is_file()
+        ):
+            self.name = data_file_prefix_name
+            self.features = pd.read_csv(features_dir, index_col=0)
+            self.labels = pd.read_csv(labels_dir, index_col=0)
+        else:
+            raise FileNotFoundError(
+                f"""One of the dataset files cannot be found.
+                - features: {features_dir}
+                - labels: {labels_dir}
+            """
+            )
+
+    def get_X(self) -> np.ndarray:
+        """obtain a column-vector matrix of the dataset features"""
+        return self.features.to_numpy()
+
+    def get_Y(self, colname: str) -> np.ndarray:
+        """obtain a single column vector for all labels of a column"""
+        return self.labels[colname].to_numpy().reshape(-1, 1)
+
     def export(self, folder: pathlib.Path, dataset_name: str) -> None:
         """export the dataset features and labels to CSV files"""
         self.features.to_csv(
@@ -38,34 +73,57 @@ class GPDataset:
             pathlib.Path.joinpath(folder, f"{self.name}__{dataset_name}_labels.csv")
         )
 
-    def standard_scale(self) -> StandardScaler:
+    def standard_scale(self) -> Tuple[StandardScaler, StandardScaler]:
         """standard scale this dataset
 
         returns the fitted `sklearn.StandardScaler` which can then be used to rescale the data
         """
-        scaler = StandardScaler()
-        self.features[self.features.columns] = scaler.fit_transform(
+        feature_scaler = StandardScaler()
+        label_scaler = StandardScaler()
+        self.features[self.features.columns] = feature_scaler.fit_transform(
             self.features[self.features.columns]
         )
-        self.labels[self.labels.columns] = scaler.fit_transform(
+        self.labels[self.labels.columns] = label_scaler.fit_transform(
             self.labels[self.labels.columns]
         )
         # return the fitted scaler
-        return scaler
+        return (feature_scaler, label_scaler)
 
-    def rescale(self, fitted_scaler: StandardScaler) -> None:
+    def rescale(
+        self,
+        fitted_feature_scaler: StandardScaler,
+        fitted_label_scaler: StandardScaler,
+    ) -> None:
         """rescale a transformed dataset given a fitted `sklearn.StandardScaler`
 
         performs in-place rescaling of the dataset
         """
-        self.features[self.features.columns] = fitted_scaler.fit_transform(
+        self.features[self.features.columns] = fitted_feature_scaler.inverse_transform(
             self.features[self.features.columns]
         )
-        self.labels[self.labels.columns] = fitted_scaler.fit_transform(
+        self.labels[self.labels.columns] = fitted_label_scaler.inverse_transform(
             self.labels[self.labels.columns]
         )
-        # return the fitted scaler
-        return fitted_scaler
+
+    def print_info(self) -> None:
+
+        rows, *_ = self.features.shape
+
+        print(
+            f"""
+---
+GP Dataset: "{self.name}"
+
+    - feature columns:
+{pformat(self.features.columns.to_list(), indent=8)}
+
+    - label columns:
+{pformat(self.labels.columns.to_list(), indent=8)}
+
+    - total vector entries: {rows}
+---
+"""
+        )
 
 
 # function typing used to encode messages into GPR feature vectors