d664a92c9835210ceeb433b17883bbd225d6db59
Roman Stadlhuber <rs@d-aria.at>
2023-01-04 22:18:20 +0100

    extend plotting capabilities add a utility class to generate traces fit the plot using the script

--- gpr_misc/plot_trajectories.py
+++ gpr_misc/plot_trajectories.py
@@ -1,3 +1,4 @@
+from plotters import TrajectoryPlotter
 from helper_types import GPDataset
 import plotly.graph_objects as go
 import pathlib
@@ -6,21 +7,65 @@ import pathlib
 if __name__ == "__main__":
     # NOTE: assume the column names are the same for all datasets
     path_cw = pathlib.Path("./data/trajectories/clockwise")
+    path_ccw = pathlib.Path("./data/trajectories/counterclockwise")
+    path_eval = pathlib.Path("./data/trajectories/eval")
 
-    dataset_cw = GPDataset.load(dataset_folder=path_cw, name="Clockwise trajectory")
+    dataset_cw = GPDataset.load(dataset_folder=path_cw, name="clockwise trajectory")
     # the diff drive estimates are the features
     trajectory_cw_odom = dataset_cw.features
     # the ground truth are the labels
     trajectory_cw_groundtruth = dataset_cw.labels
+    color_cw = "blue"
+
+    dataset_ccw = GPDataset.load(
+        dataset_folder=path_ccw, name="counter-clockwise trajectory"
+    )
+    # the diff drive estimates are the features
+    trajectory_ccw_odom = dataset_ccw.features
+    # the ground truth are the labels
+    trajectory_ccw_groundtruth = dataset_ccw.labels
+    color_ccw = "green"
+
+    dataset_eval = GPDataset.load(dataset_folder=path_eval, name="clockwise trajectory")
+    # the diff drive estimates are the features
+    trajectory_eval_odom = dataset_eval.features
+    # the ground truth are the labels
+    trajectory_eval_groundtruth = dataset_eval.labels
+    color_eval = "red"
+
+    # NOTE: here I deliberately switched x and y to gain space for the legend and
+    # the positions do not matter since only deltas are used
 
     fig = go.Figure()
-    fig.add_trace(
-        go.Scatter(
-            x=trajectory_cw_odom["pose2d.x (/odom)"].to_numpy(),
-            y=trajectory_cw_odom["pose2d.y (/odom)"].to_numpy(),
-            line=dict(color="blue"),
-            showlegend=False,
-        )
+    plotter = TrajectoryPlotter(fontsize=18)
+    lineplot_cw = plotter.line_trace(
+        y=trajectory_cw_odom["pose2d.x (/odom)"].to_numpy(),
+        x=trajectory_cw_odom["pose2d.y (/odom)"].to_numpy(),
+        color=color_cw,
+        name="Training trajectory (clockwise and counter-clockwise)",
     )
-    print("plotting")
+    lineplot_ccw = plotter.line_trace(
+        y=trajectory_ccw_odom["pose2d.x (/odom)"].to_numpy(),
+        x=trajectory_ccw_odom["pose2d.y (/odom)"].to_numpy(),
+        color=color_ccw,
+        name="counter clockwise",
+    )
+    lineplot_eval = plotter.line_trace(
+        y=trajectory_eval_odom["pose2d.x (/odom)"].to_numpy(),
+        x=trajectory_eval_odom["pose2d.y (/odom)"].to_numpy(),
+        color=color_eval,
+        name="Evaluation trajectory",
+    )
+    fig.add_trace(lineplot_cw)
+    # fig.add_trace(lineplot_ccw)
+    fig.add_trace(lineplot_eval)
+    plotter.format_figure(
+        fig,
+        x_title="x [m]",
+        y_title="y [m]",
+        y_range=(-2, 2.5),
+        # x_range=(-1.5, 5.5),
+        width_px=800,
+        height_px=600,
+    )
     fig.show()
---
+++ gpr_misc/plotters.py
@@ -0,0 +1,138 @@
+from typing import Tuple, Optional
+import plotly.graph_objects as go
+from enum import Enum
+import numpy as np
+
+
+class LineType(Enum):
+    """A convenience wrapper around plotlys `dash` prop."""
+
+    DASHED = "dash"
+    SOLID = None
+
+
+class TrajectoryPlotter:
+    """Class to generate traces on and format a plot.
+
+
+    Usage:
+    ```python
+    fig = go.Figure()
+    plotter = TrajectoryPlotter(...) # instantiate the plotter
+    # plot some traces
+    line = plotter.line_trace(...)
+    markers = plotter.marker_trace(...)
+    # add the traces to the figure in the desired order
+    fig.add_trace(line)
+    fig.add_trace(markers)
+    # format the figure
+    plotter.format_figure(...)
+    # show the figure
+    fig.show()
+    ```
+    """
+
+    def __init__(self, dash_width: float = 3.5, fontsize: float = 14) -> None:
+        self.dash_width = dash_width
+        self.fontsize = fontsize
+
+    def line_trace(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        color: str,
+        line_type: LineType = LineType.SOLID,
+        show_legend: bool = True,
+        name: str = "",
+    ) -> go.Scatter:
+        """draws a dashed line plot"""
+        return go.Scatter(
+            x=x,
+            y=y,
+            line=dict(color=color, width=self.dash_width, dash=line_type.value),
+            showlegend=show_legend,
+            name=name,
+        )
+
+    def marker_trace(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        symbol: str,
+        color: str,
+        name: str,
+        marker_size: float,
+        marker_outline_width: float,
+    ) -> go.Scatter:
+        """draws a scatter plot using marker symbols"""
+        num_markers, *_ = np.shape(x)
+        return go.Scatter(
+            mode="markers",
+            x=x,
+            y=y,
+            marker_symbol=np.repeat(symbol, num_markers),
+            marker_color=color,
+            marker_line_color="black",
+            marker_line_width=marker_outline_width,
+            marker_size=marker_size,
+            name=(name),
+        )
+
+    def format_figure(
+        self,
+        fig,
+        x_title: str,
+        y_title: str,
+        x_range: Optional[Tuple[float, float]] = None,
+        y_range: Optional[Tuple[float, float]] = None,
+        width_px: Optional[int] = None,
+        height_px: Optional[int] = None,
+    ) -> None:
+        """styles the plot according to the paper style-guidelines"""
+        fig.update_layout(
+            # move the legend to the top right
+            legend=dict(
+                yanchor="top",  # ?
+                y=0.99,  # y relative position
+                xanchor="right",  # ?
+                x=0.99,  # x relative position
+                bgcolor="lightgray",  # backround color for the legend
+                bordercolor="black",  # border color
+                borderwidth=1,  # border width
+                font=dict(size=self.fontsize),
+            ),
+            paper_bgcolor="rgba(0,0,0,0)",  # set background transparent ...
+            plot_bgcolor="rgba(0,0,0,0)",  # ... (required for prints)
+            # LaTeX axis titles
+            yaxis_title=y_title,
+            xaxis_title=x_title,
+            # plot dimensions
+            height=height_px,
+            width=width_px,
+            font=dict(size=self.fontsize),
+            xaxis_range=x_range,
+            yaxis_range=y_range,
+        )
+        # NOTE: zero-lines are styled separately, see:
+        # https://plotly.com/python/axes/#styling-and-coloring-axes-and-the-zeroline
+        fig.update_xaxes(
+            mirror=True,
+            ticks="outside",
+            showline=True,
+            gridcolor="black",
+            linecolor="black",
+            zerolinecolor="black",  # separate zero line styling
+            zerolinewidth=1,
+        )
+        fig.update_yaxes(
+            mirror=True,  # idk why but its needed
+            ticks="outside",
+            showline=True,
+            gridcolor="black",  # grid
+            linecolor="black",  # outline
+            zerolinecolor="black",
+            zerolinewidth=1,
+            # these two settings make the axes have the same scale
+            scaleanchor="x",
+            scaleratio=1,
+        )
