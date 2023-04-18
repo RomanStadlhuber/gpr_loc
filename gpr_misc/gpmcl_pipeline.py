d5ab2dcf52699c28d06f31d26ba17cc007606e82
Roman Stadlhuber <roman.stadlhuber@iviso.at>
2023-04-19 00:44:46 +0200

    add gpmcl pipeline script will be used to invoke and glue together components of the localization

---
+++ gpr_misc/gpmcl_pipeline.py
@@ -0,0 +1,46 @@
+from gpmcl.scan_tools_3d import ScanTools3D
+from gpmcl.localization_scenario import (
+    LocalizationScenario,
+    LocalizationScenarioConfig,
+    LocalizationSyncMessage,
+    LocalizationPipeline,
+)
+import numpy as np
+import pathlib
+
+
+# TODO:
+# the pipeline will later use the following modules
+# - Odometry Module (which will use GP inference)
+# - Mapping Module (instead of ScanTools3D, which will be internal)
+# - Filtering Module (which will glue Odometry and Mapping Together)
+
+
+class GPMCLPipeline(LocalizationPipeline):
+    """Pipeline for Gaussian Process Monte Carlo Localization
+
+    Uses a Gaussian Process Particle Filter (GP-PF).
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def inference(self, synced_msgs: LocalizationSyncMessage, timestamp: int) -> None:
+        pcd = ScanTools3D.scan_msg_to_open3d_pcd(synced_msgs.scan_3d)
+        print(f"[{timestamp}]: Got PCD with {np.shape(np.asarray(pcd.points))} points!")
+
+
+if __name__ == "__main__":
+    # instantiate the pipeline
+    pipeline = GPMCLPipeline()
+    # configure the scenario
+    scenario_config = LocalizationScenarioConfig(
+        bag_path=pathlib.Path("./bags/explore.bag"),  # TODO: make this configurable
+        topic_odom_est="/odom",
+        topic_scan_3d="/velodyne_points",
+        localization_pipeline=pipeline,
+    )
+    # instantiate the scenario
+    localization_scenario = LocalizationScenario(config=scenario_config)
+    # run localization inference
+    localization_scenario.spin_bag()
