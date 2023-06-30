d89f2fcf40fa6b948bda6c286fc10dbc7888c9af
Roman Stadlhuber <roman.stadlhuber@iviso.at>
2023-07-01 00:23:01 +0200

    replace all dependencies on ROS libraries remove ros_numpy and all invocations to it this also removes all ROS-native message imports the code is now ROS-free again also, name the Open3d window and have it open at a smaller size

--- gpr_misc/gpmcl/scan_tools_3d.py
+++ gpr_misc/gpmcl/scan_tools_3d.py
@@ -1,14 +1,10 @@
 from rosbags.typesys.types import (
     sensor_msgs__msg__PointCloud2 as PointCloud2,
-    sensor_msgs__msg__PointField as PointField,
-    std_msgs__msg__Header as Header,
 )
-from typing import Optional, List
+from typing import List
 import numpy as np
-import ros_numpy
-import sensor_msgs.msg
-import std_msgs.msg
 import open3d
+import struct
 
 # TODO: make this work ...
 # import ros_numpy
@@ -18,113 +14,36 @@ class ScanTools3D:
     """Utility class for converting and processing 3D Scan data."""
 
     @staticmethod
-    def visualize_pcd(pcd: open3d.geometry.PointCloud) -> None:
-        """A blocking call to Open3Ds visualization pipeline."""
-        open3d.visualization.draw_geometries(
-            [pcd],
-            front=[-0.86620647140619078, -0.23940427344046508, 0.43860226031391952],
-            lookat=[-1.9334621429443359, 5.630396842956543, 0.42972373962402344],
-            up=[0.31166516566442265, 0.42724517318096378, 0.84872044072529351],
-            zoom=0.49999999999999978,
-        )
+    def pointcloud2_to_open3d_pointcloud(ros_pcd: PointCloud2) -> open3d.geometry.PointCloud:
+        """Convert a `sensor_msgs/PointCloud2` to an `open3d.geometry.PointCloud`.
 
-    @staticmethod
-    def visualize_scene(
-        scan_pcd: open3d.geometry.PointCloud,
-        map_pcd: open3d.geometry.PointCloud,
-        feature_inlier_pcd: open3d.geometry.PointCloud,
-        feature_outlier_pcd: Optional[open3d.geometry.PointCloud],
-    ) -> None:
-        # set colors of the output PCDs
-        ScanTools3D.__set_pcd_color(scan_pcd, 0.6 * np.ones((3)))  # grey
-        ScanTools3D.__set_pcd_color(map_pcd, np.array([0, 0, 1.0]))  # blue
-        ScanTools3D.__set_pcd_color(feature_inlier_pcd, np.array([0, 1.0, 0]))  # green
-        pcds = [scan_pcd, map_pcd, feature_inlier_pcd]
-        if feature_outlier_pcd is not None:
-            ScanTools3D.__set_pcd_color(feature_outlier_pcd, np.array([1.0, 0, 0]))  # red
-            pcds = [scan_pcd, feature_outlier_pcd, map_pcd, feature_inlier_pcd]
-        open3d.visualization.draw_geometries(
-            pcds,
-            front=[-0.86620647140619078, -0.23940427344046508, 0.43860226031391952],
-            lookat=[5.5199312311289148, -7.983294179212173, 7.7187918904921293],
-            up=[0, 0, 1],
-            zoom=0.1799999999999996,
-        )
-
-    @staticmethod
-    def scan_msg_to_open3d_pcd(
-        msg: PointCloud2,
-        scan_tf: Optional[np.ndarray] = None,
-    ) -> open3d.geometry.PointCloud:
-        """convert a rosbags `PointCloud2` message to an Open3D `PointCloud` object
-
-        Optionally, use `scan_tf` (4x4 transformation) to transform the point cloud into the desired frame.
+        ### Remark
+        This method was adapted from
+        [Open3D-SLAMs Consversion](https://github.com/leggedrobotics/open3d_slam/blob/07a37d012516e2baac6588220c3d421e55e26e0d/open3d_utils/open3d_conversions/src/open3d_conversions.cpp#L59).
         """
-        # convert message to numpy array
-        # TODO: make this work
-        ros_msg = ScanTools3D.__to_rospcd(msg)
-        # an intermediate representation of the point cloud with a weird type and additional information
-        pcd_intermediate = ros_numpy.numpify(ros_msg)
-        # convert the intermediate representation to (N x 3) points-matrix
-        points = ScanTools3D.__numpyfied_to_xyz(pcd_intermediate)
-        # see: http://www.open3d.org/docs/release/python_api/open3d.geometry.PointCloud.html#open3d.geometry.PointCloud.__init__
-        # and: >>> help(open3d.cpu.pybind.utility.Vector3dVector)
-        pcd = open3d.geometry.PointCloud(points=open3d.cpu.pybind.utility.Vector3dVector(points))
-        if scan_tf is not None:
-            pcd.transform(scan_tf)
-        return pcd
-
-    @staticmethod
-    def __to_rospcd(msg: PointCloud2) -> sensor_msgs.msg.PointCloud2:
-        """Convert PointCloud2 from `rosbags` to actual ROS message"""
-        pcd_msg = sensor_msgs.msg.PointCloud2()
-        pcd_msg.header = ScanTools3D.__to_ros_header(msg.header)
-        pcd_msg.is_bigendian = msg.is_bigendian
-        pcd_msg.point_step = msg.point_step
-        pcd_msg.row_step = msg.row_step
-        pcd_msg.data = msg.data
-        pcd_msg.width = msg.width
-        pcd_msg.height = msg.height
-        # convert point fields
-        point_fields = list(map(ScanTools3D.__to_ros_point_field, msg.fields))
-        pcd_msg.fields = point_fields
-        return pcd_msg
-
-    @staticmethod
-    def __to_ros_point_field(point_field: PointField) -> sensor_msgs.msg.PointField:
-        ros_pointfield = sensor_msgs.msg.PointField()
-        ros_pointfield.count = point_field.count
-        ros_pointfield.datatype = point_field.datatype
-        ros_pointfield.name = point_field.name
-        ros_pointfield.offset = point_field.offset
-        return ros_pointfield
-
-    @staticmethod
-    def __to_ros_header(header: Header) -> std_msgs.msg.Header:
-        ros_header = std_msgs.msg.Header()
-        ros_header.frame_id = header.frame_id
-        ros_header.stamp.nsecs = header.stamp.nanosec
-        ros_header.stamp.secs = header.stamp.sec
-        return ros_header
-
-    @staticmethod
-    def __numpyfied_to_xyz(points: np.ndarray):
-        """Convert the result of `ros_numpy.numpify` to an `(N x 3)` matrix.
-
-        `ros_numpy.numpify(point_cloud_2)` outputs a single-dimensional array of size `N` (number of points).
-        Each entry is of the shape `(x, y, z, intensity, ring, theta)` and of type `numpy.void`.
-        To obtain the x y and z values from an element, it needs to be adressed as `(pt[0], pt[1], pt[2])`.
-
-        The output of this function can be used to set the points of an `open3d.geometry.PointCloud`.
-        """
-        num_pts, *_ = np.shape(points)
-        xyz = np.zeros((num_pts, 3), dtype=np.float64)
-
-        for idx, pt in enumerate(points):
-            point = np.array([pt[0], pt[1], pt[2]], dtype=np.float64)
-            xyz[idx, :] = point
-
-        return xyz
+        # the raw data describing the point cloud
+        data_raw = ros_pcd.data
+        # fields describe how the raw data is aligned to represent individual points
+        data_fields = ros_pcd.fields
+        # stride is the byte offset between start of a new point unit
+        stride = ros_pcd.point_step
+        # load the data-offsets for the x, y and z fields
+        offset_x = next(f.offset for f in data_fields if f.name == "x")
+        offset_y = next(f.offset for f in data_fields if f.name == "y")
+        offset_z = next(f.offset for f in data_fields if f.name == "z")
+        points_xyz = np.empty((ros_pcd.width * ros_pcd.height, 3), dtype=np.float32)
+        idx_pt = 0
+        for i in range(0, len(data_raw), stride):
+            # unpacking, where "f" means to interpet as 32-bit floating point data
+            # the result of unpack_from is always a tuple, but we're just interested in the first entry
+            # see: https://docs.python.org/3.8/library/struct.html#struct.unpack_from
+            x, *_ = struct.unpack_from("f", data_raw, i + offset_x)
+            y, *_ = struct.unpack_from("f", data_raw, i + offset_y)
+            z, *_ = struct.unpack_from("f", data_raw, i + offset_z)
+            points_xyz[idx_pt] = np.array([x, y, z])
+            idx_pt += 1
+        pcd_o3d = open3d.geometry.PointCloud(open3d.utility.Vector3dVector(points_xyz))
+        return pcd_o3d
 
     @staticmethod
     def __set_pcd_color(pcd: open3d.geometry.PointCloud, color: np.ndarray) -> None:
@@ -145,7 +64,9 @@ class PointCloudVisualizer:
     def update(self, pcds: List[open3d.geometry.PointCloud]) -> None:
         """Update the visualizer with a (consistent) set of point clouds."""
         if not self.started:
-            self.vis.create_window()
+            # create a named window that is smaller than your average screen
+            # I found it to be quite annoying when it's too large
+            self.vis.create_window(window_name="3D Scan Map", width=800, height=600)
             self.started = True
         else:
             self.__update_pcds(pcds)
--- gpr_misc/gpmcl_pipeline.py
+++ gpr_misc/gpmcl_pipeline.py
@@ -61,7 +61,7 @@ class GPMCLPipeline(LocalizationPipeline):
         )
 
     def inference(self, synced_msgs: LocalizationSyncMessage, timestamp: int) -> None:
-        pcd_scan = ScanTools3D.scan_msg_to_open3d_pcd(synced_msgs.scan_3d)
+        pcd_scan = ScanTools3D.pointcloud2_to_open3d_pointcloud(synced_msgs.scan_3d)
         self.mapper.compute_scan_correspondences_to_map(pcd_scan)
         if synced_msgs.groundtruth is not None:
             T_curr = odometry_msg_to_affine_transform(synced_msgs.groundtruth)
