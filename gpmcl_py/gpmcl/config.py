5a72e97c3304fe664a684dcb39b61c9fd1f0d87a
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2023-07-08 21:12:08 +0200

    add config typings & example for new system

---
+++ gpmcl_py/gpmcl/config.py
@@ -0,0 +1,63 @@
+from typing import TypedDict, List, Optional
+import pathlib
+import yaml
+
+
+class ScanTFConfig(TypedDict):
+    position: List[float]
+    orientation: List[float]
+
+
+class MotionComponentLabelConfig(TypedDict):
+    x: str
+    y: str
+    theta: str
+
+
+class MotionModelGPConfig(TypedDict):
+    model_dir: str
+    estimated_motion_labels: MotionComponentLabelConfig
+    previous_motion_labels: MotionComponentLabelConfig
+
+
+class MapperConfig(TypedDict):
+    # the following parameters are required for keypoint detection
+    eig_ratio_32: float
+    eig_ratio_21: float
+    min_neighbor_count: int
+    voxel_size: float
+    scan_tf: ScanTFConfig
+    # parameter for correspondence estimation using KDTree-FLANN
+    kdtree_search_radius: float  # the nearest neighbor search radius
+
+
+class FastSLAMConfig(TypedDict):
+    # ignore features and landmarks beyond this range
+    max_feature_range: float  # the max distance in which features are observed
+    # these parameters are related to Fast SLAM
+    max_unobserved_count: int
+    particle_count: int
+    keypoint_covariance: List[int]  # covariance of an XYZ keypoint
+    observation_covariance: List[int]  # signal cov. of range-bearing observations
+
+
+class BagRunnerConfig(TypedDict):
+    bag_path: str
+    estimated_odometry_topic: str
+    pointcloud_topic: str
+    groundtruth_topic: Optional[str]
+    sync_period: float
+    stop_after: Optional[int]
+
+
+class GPMappingOfflineConfig(TypedDict):
+    mapper: MapperConfig
+    fast_slam: FastSLAMConfig
+    motion_model_gp: MotionModelGPConfig
+    bag_runner: BagRunnerConfig
+
+
+def load_gpmapping_offline_config(config_path: pathlib.Path) -> GPMappingOfflineConfig:
+    with open(config_path, "r") as f_conf:
+        conf: GPMappingOfflineConfig = yaml.safe_load(stream=f_conf)
+        return conf
