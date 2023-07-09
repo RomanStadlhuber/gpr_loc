9ac67df42e1d930f92033d7c8809036323b551b7
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2023-07-09 11:20:32 +0200

    add class for GP motion prediction

---
+++ gpmcl_py/gpmcl/motion_model.py
@@ -0,0 +1,56 @@
+from gpmcl.helper_types import GPModelSet, GPDataset
+from gpmcl.config import MotionModelGPConfig
+import pandas as pd
+import numpy as np
+import pathlib
+
+
+class MotionModel:
+    def __init__(self, config: MotionModelGPConfig) -> None:
+        self.config = config
+        self.colnames_estimated_motion = [
+            config["estimated_motion_labels"]["x"],
+            config["estimated_motion_labels"]["y"],
+            config["estimated_motion_labels"]["theta"],
+        ]
+        self.colnames_previous_motion = [
+            config["previous_motion_labels"]["x"],
+            config["previous_motion_labels"]["y"],
+            config["previous_motion_labels"]["theta"],
+        ]
+
+        # region: load GP models
+        model_path = pathlib.Path(config["model_dir"])
+        if not model_path.exists() or not model_path.is_dir():
+            raise FileNotFoundError(
+                f"""
+Unable to load GP Models!
+'{model_path}' does not exist or is not a directory.
+            """
+            )
+        else:
+            self.models = GPModelSet.load_models(model_path)
+        # endregion: load GP models
+
+    def regression(
+        self,
+        estimated_motion: np.ndarray,
+        previous_motion: np.ndarray,
+    ) -> np.ndarray:
+        """Perform motion prediction using Gaussian process regression.
+
+        ### Remark
+        The input values can have dimensions `(3x1)` or `(Nx3)`,
+        but they are required to have identical dimensions.
+        """
+        # create dataframes with the required column names for regression
+        features_est = pd.DataFrame(columns=self.colnames_estimated_motion, data=estimated_motion)
+        features_prev = pd.DataFrame(columns=self.colnames_previous_motion, data=previous_motion)
+        features = features_est.join(features_prev)
+        # create an unnamed dataset without labels
+        D_in = GPDataset(name="", features=features, labels=pd.DataFrame())
+        # pass the unscaled dataset to the regression method.
+        # it will inernally scale the dataset accordingly
+        D_regr = self.models.perform_regression(D_test_unscaled=D_in)
+        # return the predicted motion mean vectors
+        return D_regr.labels.to_numpy()
