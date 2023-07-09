b7a91075297ec0cf584bc937ed4264e3e83d92c0
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2023-07-09 11:41:12 +0200

    implement motion prediction from GP

--- gpmcl_py/gpmcl/fast_slam.py
+++ gpmcl_py/gpmcl/fast_slam.py
@@ -2,15 +2,13 @@ from gpmcl.transform import Pose2D
 from gpmcl.config import FastSLAMConfig
 from gpmcl.particle import FastSLAMParticle
 from gpmcl.motion_model import MotionModel
-from typing import List
+from typing import List, Tuple
 import numpy as np
 import scipy.stats
 import open3d
 
 
 class FastSLAM:
-    # TODO: how to manage particle values?
-
     def __init__(self, config: FastSLAMConfig, motion_model: MotionModel) -> None:
         # initialize particles by setting landmarks empty
         # and sampling about intial guess with uniform distribution
@@ -18,32 +16,46 @@ class FastSLAM:
         self.config = config
         self.M = self.config["particle_count"]
         self.particles: List[FastSLAMParticle] = []
+        # initialize all particle weights equally likely
+        self.ws = 1 / self.M * np.ones(self.M)
         self.motion_model = motion_model
+        self.previous_motion = np.zeros((self.M, 3), dtype=np.float64)
         pass
 
     def initialize_from_pose(self, x0: np.ndarray) -> None:
         """Initialize the particle set by sampling about a pose."""
         # TODO: configure initial sampling radius
-        self.particles = self.sample_circular_uniform(initial_guess=x0)
+        self.particles, self.ws = self.sample_circular_uniform(initial_guess=x0)
 
-    def predict(self, u: np.ndarray) -> None:
-        # NOTE: "u" is the motion delta in the robot frame
+    def predict(self, estimated_motion: np.ndarray) -> None:
         # predict new particle poses
         # update the trajectories by appending the prior
-        pass
+        predicted_motion = self.motion_model.predict(
+            estimated_motion=np.repeat(estimated_motion, repeats=self.M, axis=0),
+            previous_motion=self.previous_motion,
+        )
+        self.previous_motion = predicted_motion
+        for idx, particle in enumerate(self.particles):
+            # TODO: add noise according to predicted covaraince
+            particle.apply_u(predicted_motion[idx])
 
     def update(self, keypoints: open3d.geometry.PointCloud) -> None:
         """Update the particle poses and landmarks from observed keypoints."""
         # detect keypoints
         # update landmarks
         # compute particle likelihoods
+        # normalize weights
         # resmple particles
         pass
 
-    def sample_circular_uniform(self, initial_guess: np.ndarray, circle_radius: float = 1.0) -> List[FastSLAMParticle]:
+    def sample_circular_uniform(
+        self, initial_guess: np.ndarray, circle_radius: float = 1.0
+    ) -> Tuple[List[FastSLAMParticle], np.ndarray]:
         """Sample **new** particles about an initial guess pose.
 
         This **resets** the entire particle and map state.
+
+        Returns particles and their weights
         """
         ranges = scipy.stats.uniform.rvs(scale=circle_radius, size=self.M)
         bearings = scipy.stats.uniform.rvs(loc=-np.pi, scale=2 * np.pi, size=self.M)
@@ -57,4 +69,5 @@ class FastSLAM:
             )
         )
         particles = list(map(lambda vec: FastSLAMParticle(x=Pose2D.from_twist(vec)), poses))
-        return particles
+        ws = 1 / self.M * np.ones(self.M)
+        return (particles, ws)
