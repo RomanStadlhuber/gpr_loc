ea8b566a0ca5cb9060cbcef7958347eb2680cb59
Roman Stadlhuber <roman.stadlhuber@iviso.at>
2023-07-12 15:10:05 +0200

    implement correspondence estimation for a particle

--- gpmcl_py/gpmcl/particle.py
+++ gpmcl_py/gpmcl/particle.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from gpmcl.transform import Pose2D
 import numpy as np
+import numpy.typing as npt
+import open3d
 
 
 @dataclass
@@ -28,7 +30,7 @@ class FastSLAMParticle:
         # apply the motion to obtain the new pose
         self.x.perturb(u)
 
-    def update_landmark(self, idx: int, delta: np.ndarray, K_gain: np.ndarray, H: np.ndarray, Q: np.ndarray) -> None:
+    def update_landmark(self, idx: int, delta: np.ndarray, K_gain: np.ndarray, H: np.ndarray, Qz: np.ndarray) -> None:
         """Update the position of an individual landmark given its index and Kalman filter values"""
         # TODO: normalize the angle values of the delta vector to [-pi, pi]!
         self.landmarks[idx] += K_gain @ delta
@@ -36,7 +38,7 @@ class FastSLAMParticle:
         J = np.eye(3) - K_gain * H
         P = self.landmark_covariances[idx]
         # update landmark position covariance
-        self.landmark_covariances[idx] = J @ P @ J.T + K_gain @ Q @ K_gain.T
+        self.landmark_covariances[idx] = J @ P @ J.T + K_gain @ Qz @ K_gain.T
 
     def add_landmarks(self, ls: List[np.ndarray], Q_0: np.ndarray, Qs: Optional[np.ndarray] = None) -> None:
         """Add a set of landmarks with common or individual covariances.
@@ -48,3 +50,34 @@ class FastSLAMParticle:
             self.landmark_covariances = np.vstack((self.landmark_covariances, np.repeat([Q_0], repeats=len(ls))))
         else:
             self.landmark_covariances = np.vstack((self.landmark_covariances, Qs))
+
+    def estimate_correspondences(
+        self, pcd_keypoints: open3d.geometry.PointCloud, max_distance: float = 0.6
+    ) -> Tuple[npt.NDArray[np.int32], open3d.geometry.PointCloud]:
+        """Estimate the correspondences between observed keypoints and landmarks in the map.
+
+        ### Remarks
+
+        This creates a copy of `pcd_keypoints` in order to transform the keypoints into the map frame and
+        perofrm KDTree radius search on them.
+        """
+        # copy the original pointcloud
+        pcd_keypoints = open3d.geometry.PointCloud(pcd_keypoints)
+        # transform the keypoints into the map frame
+        # this is done under the assumptions that, at some point, there are more landmarks than keypoints being observed
+        pcd_keypoints.transform()
+        n_landmarks, *_ = self.landmarks.shape
+        if n_landmarks == 0:
+            return (np.empty((0, 2), dtype=np.int32), pcd_keypoints)
+        else:
+            # create a KD-Tree for correspondence search
+            kdtree_keypoints = open3d.geometry.KDTreeFLANN(pcd_keypoints)
+            correspondences = np.empty((n_landmarks, 2), dtype=np.int32)
+            for idx_l, landmark in enumerate(self.landmarks):
+                # (num_neighbors, idxs, distances) = ...
+                # see: http://www.open3d.org/docs/latest/python_api/open3d.geometry.KDTreeFlann.html#open3d.geometry.KDTreeFlann.search_radius_vector_3d
+                [_, idxs, distances] = kdtree_keypoints.search_radius_vector3d(query=landmark, radius=max_distance)
+                idx_min_dist = np.argmin(distances)
+                # set min-distance point to be the corresponding
+                correspondences[idx_l] = [idx_l, idxs[idx_min_dist]]
+            return (correspondences, pcd_keypoints)
