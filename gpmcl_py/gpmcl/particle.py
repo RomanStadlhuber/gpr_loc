2fd948e9df6e3c9fa924acf63567435ffac186bf
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2023-07-14 12:42:21 +0200

    begin refactoring Fast SLAM particle class

--- gpmcl_py/gpmcl/particle.py
+++ gpmcl_py/gpmcl/particle.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
-from typing import List, Optional, Tuple
+from typing import Optional, Tuple
+from gpmcl.observation_model import range_bearing_observation_landmark, range_bearing_observation_keypoint
 from gpmcl.transform import Pose2D
+from autograd import jacobian
 import numpy as np
 import numpy.typing as npt
 import open3d
@@ -30,27 +32,6 @@ class FastSLAMParticle:
         # apply the motion to obtain the new pose
         self.x.perturb(u)
 
-    def update_landmark(self, idx: int, delta: np.ndarray, K_gain: np.ndarray, H: np.ndarray, Qz: np.ndarray) -> None:
-        """Update the position of an individual landmark given its index and Kalman filter values"""
-        # TODO: normalize the angle values of the delta vector to [-pi, pi]!
-        self.landmarks[idx] += K_gain @ delta
-        # update landmark position mean
-        J = np.eye(3) - K_gain * H
-        P = self.landmark_covariances[idx]
-        # update landmark position covariance
-        self.landmark_covariances[idx] = J @ P @ J.T + K_gain @ Qz @ K_gain.T
-
-    def add_landmarks(self, ls: np.ndarray, Q_0: np.ndarray, Qs: Optional[np.ndarray] = None) -> None:
-        """Add a set of landmarks with common or individual covariances.
-
-        Pass `Qs` as individual landmark covariances.
-        """
-        self.landmarks = np.vstack((self.landmarks, ls))
-        if Qs is None:
-            self.landmark_covariances = np.vstack((self.landmark_covariances, np.repeat([Q_0], repeats=len(ls))))
-        else:
-            self.landmark_covariances = np.vstack((self.landmark_covariances, Qs))
-
     def estimate_correspondences(
         self, pcd_keypoints: open3d.geometry.PointCloud, max_distance: float = 0.6
     ) -> Tuple[npt.NDArray[np.int32], open3d.geometry.PointCloud, npt.NDArray[np.int32], npt.NDArray[np.int32]]:
@@ -73,15 +54,14 @@ class FastSLAMParticle:
             return (
                 np.empty((0, 2), dtype=np.int32),
                 pcd_keypoints,
-                np.empty((0, 2), dtype=np.int32),
-                np.empty(0, dtype=np.int32),
+                np.empty((0, 2), dtype=np.int32),  # closest correspondence
+                np.linspace(0, np.asarray(pcd_keypoints).shape[0], dtype=np.int32),  # previously unseen correspondences
             )
         else:
             # create a KD-Tree for correspondence search
             kdtree_keypoints = open3d.geometry.KDTreeFLANN(pcd_keypoints)
             correspondences = np.empty((n_landmarks, 2), dtype=np.int32)
             c_distances = np.empty(n_landmarks, dtype=np.float64)
-            c_max = (-1, -1)
             for idx_l, landmark in enumerate(self.landmarks):
                 # (num_neighbors, idxs, distances) = ...
                 # see: http://www.open3d.org/docs/latest/python_api/open3d.geometry.KDTreeFlann.html#open3d.geometry.KDTreeFlann.search_radius_vector_3d
@@ -96,3 +76,75 @@ class FastSLAMParticle:
             closest_corresondence = correspondences[idx_c_min_distance]
             # TODO: obtain idxs of previously unseen features!!
             return (correspondences, pcd_keypoints, closest_corresondence, np.empty(0, dtype=np.int32))
+
+    def add_new_landmarks_from_keypoints(
+        self,
+        idxs_new_landmarks: npt.NDArray[np.int32],
+        keypoints_in_robot_frame: npt.NDArray[np.float64],
+        observation_covariance: np.array,
+    ) -> None:
+        pcd_l_new = open3d.geometry.PointCloud(
+            open3d.utility.Vector3dVector(keypoints_in_robot_frame[idxs_new_landmarks])
+        )
+        # transform the keyponits into the map frame
+        pcd_l_new.transform(self.x.as_t3d())
+        l_new = np.asarray(pcd_l_new.points)
+        self.__add_landmarks(ls=l_new, Q_0=observation_covariance)
+
+    def update_existing_landmarks(
+        self,
+        correspondences: npt.NDArray[np.int32],
+        keypoints_in_robot_frame: npt.NDArray[np.float64],
+        observation_covariance: np.ndarray,
+    ) -> None:
+        Q_z = observation_covariance
+        for idx_l, idx_kp in correspondences:
+            # observation and its corresponding jacobian
+            kp = keypoints_in_robot_frame[idx_kp]
+            z_l, H_l = self.__compute_landmark_observation(idx_l)
+            z_kp = range_bearing_observation_keypoint(kp)
+            delta_z = z_l - z_kp
+            Q_l = self.landmark_covariances[idx_l]
+            K_l = Q_l @ H_l.T @ np.linalg.inv(H_l @ Q_l @ H_l.T + Q_z)
+            self.__update_landmark(idx_l, delta=delta_z, K_gain=K_l, H=H_l, Qz=Q_z)
+
+    def register_unobserved_landmarks(self, idxs_unobserved: npt.NDArray[np.int32]) -> None:
+        # TODO: implement
+        pass
+
+    def __add_landmarks(self, ls: np.ndarray, Q_0: np.ndarray, Qs: Optional[np.ndarray] = None) -> None:
+        """Add a set of landmarks with common or individual covariances.
+
+        Pass `Qs` as individual landmark covariances.
+        """
+        self.landmarks = np.vstack((self.landmarks, ls))
+        if Qs is None:
+            self.landmark_covariances = np.vstack((self.landmark_covariances, np.repeat([Q_0], repeats=len(ls))))
+        else:
+            self.landmark_covariances = np.vstack((self.landmark_covariances, Qs))
+
+    def __update_landmark(self, idx: int, delta: np.ndarray, K_gain: np.ndarray, H: np.ndarray, Qz: np.ndarray) -> None:
+        """Update the position of an individual landmark given its index and Kalman filter values"""
+        # TODO: normalize the angle values of the delta vector to [-pi, pi]!
+        self.landmarks[idx] += K_gain @ delta
+        # update landmark position mean
+        J = np.eye(3) - K_gain * H
+        P = self.landmark_covariances[idx]
+        # update landmark position covariance
+        self.landmark_covariances[idx] = J @ P @ J.T + K_gain @ Qz @ K_gain.T
+
+    def __compute_landmark_observation(self, idx_landmark: int) -> Tuple[np.ndarray, np.ndarray]:
+        """Compute the observation of a landmark plus the corresponding jacobian w.r.t. the landmark.
+
+        Returns `(z, H)` where `z` is the observation vector and `H` the models jacobian w.r.t. the landmark `l`.
+        """
+
+        def h(l: np.ndarray) -> np.ndarray:
+            return range_bearing_observation_landmark(l=l, x=self.x.as_twist())
+
+        l_i = self.landmarks[idx_landmark]
+        z_i = h(l_i)
+        jacobian_of_h = jacobian(h)
+        H_i = jacobian_of_h(l_i)
+
+        return (z_i, H_i)
