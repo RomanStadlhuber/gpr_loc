ab7b32c8aebd25a9a85fe3b2f1ac8018e937389e
Roman Stadlhuber <roman.stadlhuber@gmail.com>
2023-07-15 11:13:05 +0200

    add autograd example to observation model demonstrates the usage and output

--- gpmcl_py/gpmcl/observation_model.py
+++ gpmcl_py/gpmcl/observation_model.py
@@ -1,4 +1,4 @@
-import numpy as np
+import autograd.numpy as np
 
 
 class ObservationModel:
@@ -10,7 +10,7 @@ class ObservationModel:
         dx = lx - px
         dy = ly - py
         dz = lz
-        rho = np.linalg.norm([dx, dy, dz])
+        rho = np.linalg.norm(np.array([dx, dy, dz]))
         phi = np.mod(np.arctan2(dy, dx) - pyaw, 2 * np.pi)
         psi = np.arcsin(dz / rho)
         return np.array([rho, phi, psi], dtype=np.float64)
@@ -21,3 +21,21 @@ class ObservationModel:
         phi = np.arctan2(p[1], p[0])
         psi = np.arcsin(p[2] / rho)
         return np.array([rho, phi, psi], dtype=np.float64)
+
+
+if __name__ == "__main__":
+    # test automatic differentiation:
+    from autograd import jacobian
+
+    # simulate robot pose
+    x = np.array([0.5, 0.2, np.deg2rad(30)])
+    # simulate ladnmark position
+    p_l = np.array([3, 2, 1], dtype=np.float64)
+
+    def h(l):
+        return ObservationModel.range_bearing_observation_landmark(l, x)
+
+    jacobian_of_h = jacobian(h)
+    # compute jacobian of h at l
+    Jh = jacobian_of_h(p_l)
+    print(Jh)
